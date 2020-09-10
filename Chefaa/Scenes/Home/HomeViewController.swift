fc4a383f13565e2eb479215e1fd8ebf692c042c7
KarimEbrahem <karimabdelazizmansour@gmail.com>
2020-09-10 09:26:04 +0200

    Test skeleton views while loading data

--- Chefaa/Scenes/Home/HomeViewController.swift
+++ Chefaa/Scenes/Home/HomeViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import Domain
 import RxSwift
 import RxCocoa
+import SkeletonView
 
 class HomeViewController: UIViewController {
 
@@ -18,19 +19,42 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        bindViewModel()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func bindViewModel() {
+        assert(viewModel != nil)
+        let viewWillAppear = rx.sentMessage(#selector(UIViewController.viewWillAppear(_:)))
+            .mapToVoid()
+            .asDriverOnErrorJustComplete()
+        
+        let input = HomeViewModel.Input(trigger: Driver.merge(viewWillAppear))
+        let output = viewModel.transform(input: input)
+        
+        //Bind Posts to UITableView
+//        output.posts
+//            .drive(tableView.rx.items(cellIdentifier: PostTableViewCell.reuseID,
+//        cellType: PostTableViewCell.self)) { tv, viewModel, cell in
+//            cell.bind(viewModel)
+//        }.disposed(by: disposeBag)
+        output.homeAds
+            .drive(onNext: { (homeAds: (sliders: [SliderItemViewModel], subCategories: [SubCategoryItemViewModel], brands: [BrandItemViewModel], bestSellingItems: [BestSellingItemViewModel], landingPages: [LandingPageItemViewModel])) in
+                print(homeAds.sliders.count)
+        })
+        .disposed(by: disposeBag)
+        
+        //Connect Create Post to UI
+        output.fetching
+            .drive(onNext: { [weak self] isFetching in
+                switch isFetching {
+                case true:
+                    self?.view.showAnimatedGradientSkeleton()
+                case false:
+                    self?.view.hideSkeleton()
+                }
+            })
+            .disposed(by: disposeBag)
     }
-    */
 
 }
